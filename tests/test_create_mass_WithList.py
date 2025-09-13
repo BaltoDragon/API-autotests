dfbbe9aa34f85f3243f7e363171b3863f7e6bc66
balto_dragon <a_olegka@bk.ru>
2025-09-13 21:52:20 +0300

    Add data file, refactoring

--- tests/test_create_mass_WithList.py
+++ tests/test_create_mass_WithList.py
@@ -1,38 +1,14 @@
+import data
 import requests
+import pytest
 
-BASE_URL = 'https://petstore.swagger.io/v2'
-HEADERS = {
-    'accept': 'application/json',
-    'Content-Type': 'application/json',
-}
-
-data_body_List_0 = [
-    {
-        "id": 0, "username": "MassCreateUserA", "firstName": "MyName", "lastName": "Familia",
-        "email": "example@ex.com", "password": "PASSWORD", "phone": "123456789", "userStatus": 0
-    },
-]
-data_body_List_1 = [
-    {
-        "id": 0, "username": "MassCreateUserAA", "firstName": "SurName", "lastName": "SecondName",
-        "email": "example@ex.com", "password": "PWD1234", "phone": "987654321", "userStatus": 0
-    },
-    {
-        "id": 0, "username": "MassCreateUserAB", "firstName": "", "lastName": "UniqName",
-        "email": "example@ex.com", "password": "PAROL", "phone": "192837456", "userStatus": 0
-    },
-    {
-        "id": 0, "username": "MassCreateUserAC", "firstName": "MyName", "lastName": "NoName",
-        "email": "example@ex.com", "password": "parol", "phone": "56748392", "userStatus": 0
-    }
-]
+@pytest.mark.parametrize('user_list', [data.body_empty_list, data.body_user_list, data.body_users_list])
+def test_mass_create_0(user_list):
+    url = f'{data.BASE_URL}/user/createWithList'
+    response_create = requests.post(url, headers = data.HEADERS_CREATE, json = user_list)
+    assert response_create.status_code == 200
 
 def test_mass_create_1():
-    url = f'{BASE_URL}/user/createWithList'
-    response_create = requests.post(url, headers = HEADERS, json = data_body_List_0)
-    assert response_create.status_code == 200
-
-def test_mass_create_2():
-    url = f'{BASE_URL}/user/createWithList'
-    response_create = requests.post(url, headers = HEADERS, json = data_body_List_1)
-    assert response_create.status_code == 200
+    url = f'{data.BASE_URL}/user/createWithList'
+    response_create = requests.post(url, headers = data.HEADERS_CREATE, json = data.body_users_list[1])
+    assert response_create.status_code == 400
--- tests/test_create_user.py
+++ tests/test_create_user.py
@@ -1,42 +1,14 @@
+import data
 import pytest
 import requests
 
-BASE_URL = 'https://petstore.swagger.io/v2'
-HEADERS = {
-    'accept': 'application/json',
-    'Content-Type': 'application/json',
-}
-
-data_body = [
-        #default_data
-         {
-            "id": 0, "username": "string", "firstName": "string", "lastName": "string",
-            "email": "string", "password": "string", "phone": "string", "userStatus": 0
-         },
-        #real_data
-         {
-             "id": 0, "username": "nickname", "firstName": "MyName", "lastName": "MyLastName",
-             "email": "example@ex.com", "password": "PASSWORD", "phone": "1234567890", "userStatus": 0
-         },
-        #mini_data
-         {
-             "id": 0, "username": "nickname", "firstName": "MyName",
-             "email": "example@ex.com", "password": "PASSWORD"
-         },
-        #short_string
-         {
-             "id": 0, "username": "a", "firstName": "b", "lastName": "c",
-             "email": "d@e.f", "password": "G", "phone": "12", "userStatus": 0
-         },
-        #long_string
-         {
-             "id": 0, "username": "n" * 256, "firstName": "f" * 256, "lastName": "l" * 256,
-             "email": "e" * 245 + "@ex.com", "password": "p" * 256, "phone": "1" * 256, "userStatus": 1
-         }
-        ]
-
-@pytest.mark.parametrize('testing_data', data_body)
-def test_create(testing_data):
-    url = f'{BASE_URL}/user'
-    response_create = requests.post(url, headers = HEADERS, json = testing_data)
+@pytest.mark.parametrize('testing_data', data.body_users_list)
+def test_create_0(testing_data):
+    url = f'{data.BASE_URL}/user'
+    response_create = requests.post(url, headers = data.HEADERS_CREATE, json = testing_data)
     assert response_create.status_code == 200
+
+def test_create_1():
+    url = f'{data.BASE_URL}/user'
+    response_create = requests.post(url, headers = data.HEADERS_CREATE, json = data.body_users_list)
+    assert response_create.status_code == 400
